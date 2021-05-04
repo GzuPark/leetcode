9b0efd8c8c839065c4faaf047da8c28da7326848
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2021-05-04 18:32:28 +0900

    Add challenge 20210504 solution; 210504

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 <summary>2021년 05월</summary>
 
 + [5월 3일 Running Sum of 1d Array](./challenge/202105/03_Running_Sum_of_1d_Array/main.go)
++ [5월 4일 Non-decreasing Array](./challenge/202105/04_Non-decreasing_Array/main.go)
 
 </details>
---
+++ challenge/202105/04_Non-decreasing_Array/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func checkPossibility(nums []int) bool {
+	if len(nums) <= 2 {
+		return true
+	}
+
+	var flagCnt int = 0
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > nums[i] {
+			if flagCnt == 1 {
+				return false
+			}
+
+			flagCnt += 1
+
+			if i < 2 || nums[i-2] <= nums[i] {
+				nums[i-1] = nums[i]
+			} else {
+				nums[i] = nums[i-1]
+			}
+		}
+	}
+
+	return true
+}
+
+func print(answer bool, expected bool) {
+	fmt.Printf("Answer:\t\t%t\nExpected:\t%t\n\n", answer, expected)
+}
+
+func main() {
+	nums := []int{4, 2, 3}
+	result := checkPossibility(nums)
+	print(result, true)
+
+	nums = []int{4, 2, 1}
+	result = checkPossibility(nums)
+	print(result, false)
+}
